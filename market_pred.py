6928873c735188bb19e89c3af64422211749f6f8
Sohel Tamboli <soheltamboli101@gmail.com>
2021-05-19 18:19:58 +0530

    improve code readability

--- market_pred.py
+++ market_pred.py
@@ -1,52 +1,15 @@
 import pandas as pd
 import os
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import BayesianRidge
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn import svm
-# marketData = pd.read_csv('MarketPrice/Apple_2020.csv')
-# print(marketData.sort_values(['arrival_date'])['arrival_date'])
 import pickle
-# marketSavedModel = "marketSavedModel.pkl"
+
 best =0
 uniqueAttributesInColumns =[]
-# def getAccurancyOfMarketPrice(crop_name):
-#     enc = LabelEncoder()
-#
-#     isCropPresent = False
-#     files = os.listdir('MarketPrice')
-#     for file in files:
-#         name, ext = file.split('.')
-#         if name == crop_name:
-#             isCropPresent = True
-#             break
-#     if not isCropPresent:
-#         return 0
-#
-#     marketData = pd.read_csv('MarketPrice/' + crop_name + '.csv')
-#     marketData['arrival_date'] = pd.to_datetime(marketData['arrival_date'])
-#     marketData.sort_values(['arrival_date'], inplace=True)
-#
-#     marketData['state'] = enc.fit_transform(marketData['state'])
-#     marketData['district'] = enc.fit_transform(marketData['district'])
-#     marketData['market'] = enc.fit_transform(marketData['market'])
-#     marketData['commodity'] = enc.fit_transform(marketData['commodity'])
-#     marketData['variety'] = enc.fit_transform(marketData['variety'])
-#
-#     y = marketData['modal_price']
-#     X = marketData.drop(['arrival_date', 'min_price', 'max_price', 'modal_price'], axis=1)
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
-#
-#     model = DecisionTreeRegressor()
-#     model.fit(X, y)
-#     y_pred = model.predict(X_test)
-#     # print(list(zip(y_pred,y_test)))
-#     return model.score(X_test, y_test) * 100
+
+
 def saveModels(crop_name):
     global best,uniqueAttributesInColumns
     enc = LabelEncoder()
@@ -76,35 +39,27 @@ def saveModels(crop_name):
 
     # marketData = pd.read_csv('MarketPrice/' + crop_name + '.csv')
 
-    marketData['arrival_date'] = pd.to_datetime(marketData['arrival_date'])
-    marketData.sort_values(['arrival_date'],inplace=True)
+    # marketData['arrival_date'] = pd.to_datetime(marketData['arrival_date'])
+    # marketData.sort_values(['arrival_date'],inplace=True)
 
     marketData['state']=enc.fit_transform(marketData['state'])
-    np.save('./classes/state_classes.npy', enc.classes_,allow_pickle=True)
-
+    np.save('./classes/'+crop_name+'state_classes.npy', enc.classes_,allow_pickle=True)
     marketData['district']=enc.fit_transform(marketData['district'])
-    np.save('./classes/district_classes.npy', enc.classes_,allow_pickle=True)
-
+    np.save('./classes/'+crop_name+'district_classes.npy', enc.classes_,allow_pickle=True)
     marketData['market']=enc.fit_transform(marketData['market'])
-    np.save('./classes/market_classes.npy', enc.classes_,allow_pickle=True)
-
-    marketData['commodity']=enc.fit_transform(marketData['commodity'])
-    np.save('./classes/commodity_classes.npy', enc.classes_,allow_pickle=True)
-
-    marketData['variety']=enc.fit_transform(marketData['variety'])
-    np.save('./classes/variety_classes.npy', enc.classes_,allow_pickle=True)
-
-
-
-
-
+    np.save('./classes/'+crop_name+'market_classes.npy', enc.classes_,allow_pickle=True)
+    # marketData['commodity']=enc.fit_transform(marketData['commodity'])
+    # np.save('./classes/'+crop_name+'commodity_classes.npy', enc.classes_,allow_pickle=True)
+    #
+    # marketData['variety']=enc.fit_transform(marketData['variety'])
+    # np.save('./classes/'+crop_name+'variety_classes.npy', enc.classes_,allow_pickle=True)
     y = marketData['modal_price']
-    X = marketData.drop(['arrival_date','min_price','max_price','modal_price'],axis=1)
+    X = marketData.drop(['arrival_date','min_price','max_price','modal_price','commodity','variety'],axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
 
     model = DecisionTreeRegressor()
     model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    # y_pred = model.predict(X_test)
     # print(list(zip(y_pred,y_test)))
     cur = model.score(X_test,y_test)*100
     if cur>best:
@@ -119,44 +74,45 @@ def getCurMarketPriceOfCrop(crop_name,data):
     enc = LabelEncoder()
     with open('./models/'+crop_name, 'rb') as f:
         model = pickle.load(f)
-    enc.classes_= np.load('./classes/state_classes.npy',allow_pickle=True)
+    enc.classes_= np.load('./classes/'+crop_name+'state_classes.npy',allow_pickle=True)
+    # print(enc.classes_)
+    if data['state'] not in enc.classes_:
+        return 0
     stateAfterEncoding = enc.transform([data['state']])
 
-    enc.classes_ = np.load('./classes/market_classes.npy',allow_pickle=True)
-    print(enc.classes_)
+    enc.classes_ = np.load('./classes/'+crop_name+'market_classes.npy',allow_pickle=True)
+    # print(enc.classes_)
+    if data['market'] not in enc.classes_:
+        return 0
     marketAfterEncoding = enc.transform([data['market']])
 
 
-    enc.classes_ = np.load('./classes/commodity_classes.npy',allow_pickle=True)
-    print(enc.classes_)
-    commodityAfterEncoding = enc.transform([data['commodity']])
-
-
-    enc.classes_ = np.load('./classes/variety_classes.npy',allow_pickle=True)
-    print(enc.classes_)
-    varietyAfterEncoding = enc.transform([data['variety']])
-
-
-    enc.classes_ = np.load('./classes/district_classes.npy',allow_pickle=True)
-    print(enc.classes_)
+    enc.classes_ = np.load('./classes/'+crop_name+'district_classes.npy',allow_pickle=True)
+    # print(enc.classes_)
+    if data['district'] not in enc.classes_:
+        return 0
     districtAfterEncoding = enc.transform([data['district']])
 
-
-    # stateAfterEncoding = uniqueAttributesInColumns[0].find(data['state'])
-    # districtAfterEncoding = uniqueAttributesInColumns[1].find(data['district'])
-    # marketAfterEncoding = uniqueAttributesInColumns[2].find(data['market'])
-    # commodityAfterEncoding = uniqueAttributesInColumns[3].find(data['commodity'])
-    # varietyAfterEncoding = uniqueAttributesInColumns[4].find(data['variety'])
-    data = [[stateAfterEncoding[0],districtAfterEncoding[0],marketAfterEncoding[0],commodityAfterEncoding[0],varietyAfterEncoding[0]]]
+    data = [[stateAfterEncoding[0],districtAfterEncoding[0],marketAfterEncoding[0]]]
     return model.predict(data)
 
 
-# for  i in os.listdir('MarketPrice'):
-#     globals()['best']=0
-#     name, ext = i.split('.')
-#     print(name[:-4])
-#     for j in range(50):
-#         saveModels(name[:-4])
-#     print(globals()['best'])
-# print(uniqueAttributesInColumns)
-# print(getCurMarketPriceOfCrop('apple',{'state':'Punjab','district':'Ferozpur','market':"Firozepur City",'commodity':'Apple','variety':'Other'}))
+def main():
+    typesOfCrops = set()
+    for i in os.listdir('MarketPrice'):
+        name, ext = i.split('.')
+        nameOfCrop = name[:-4]
+        typesOfCrops.add(nameOfCrop)
+    lenOfDifferentTypesOfCrop = len(typesOfCrops)
+
+    for ind,nameOfCrop in enumerate(typesOfCrops):
+
+        globals()['best'] = 0
+        for j in range(50):
+            saveModels(nameOfCrop)
+        print("--"*(ind+1)+"{} %".format(int(((ind+1)*100)/lenOfDifferentTypesOfCrop)), flush=True,end= "\r" if ind!=(lenOfDifferentTypesOfCrop-1) else "\n")
+        # print(globals()['best'])
+
+
+if __name__=='__main__':
+    main()
--- predictions.py
+++ predictions.py
@@ -1,33 +1,44 @@
+import crop_pred
 import market_pred
 import pickle
 import streamlit as st
+import pandas as pd
 import numpy as np
 # from sklearn.ensemble import RandomForestClassifier
 
-def cmp(prob):
-    return prob>=0.2
+def minimumProbabilty(prob):
+    return prob>=0.0001
 with open('./models/' + 'crop', 'rb') as f:
     model = pickle.load(f)
 
-def predict_crop(state,district,market,commodity,variety,temperature,humidity,ph,rainfall):
-    # temperature,humidity,ph,rainfall = 0,0,0,0
-    # state,district,market,commodity,variety = "","","","",""
-    # print(temperature,humidity,ph,rainfall)
+def predict_crop(state,district,market,temperature,humidity,ph,rainfall):
     temperature,humidity,ph,rainfall = float(temperature),float(humidity),float(ph),float(rainfall)
-    probabilities = model.predict_proba([[temperature,humidity,ph,rainfall]])[0]
-    classes = model.classes_
-    considered_classes = []
-    for ind,probability in enumerate(probabilities):
-        if cmp(probability):
-            considered_classes.append(classes[ind])
+    considered_classes = crop_pred.getCropsHavingMinimumProbability([temperature,humidity,ph,rainfall],minimumProbabilty)
+    # probabilities = model.predict_proba([[temperature,humidity,ph,rainfall]])[0]
+    # classes = model.classes_
+    # considered_classes = []
+    # # print(list(zip(probabilities,classes)))
+    # for ind,probability in enumerate(probabilities):
+    #     if cmp(probability):
+    #         considered_classes.append(classes[ind])
     maxRevenueGenratedClass = ""
     maxRevenue = 0
+    revenues = []
     for class_ in considered_classes:
-        curRevenue = market_pred.getCurMarketPriceOfCrop(class_,{'state':state,'district':district,'market':market,'commodity':commodity,'variety':variety})
+        curRevenue = market_pred.getCurMarketPriceOfCrop(class_,{'state':state,'district':district,'market':market})
+        # print(class_,curRevenue)
+        if not (type(curRevenue)==int or type(curRevenue)==float ):
+            curRevenue = curRevenue.tolist()
+        if(type(curRevenue)==list):
+            curRevenue =curRevenue[0]
+        print(type(curRevenue))
+        revenues.append(curRevenue)
         if maxRevenue<curRevenue:
             maxRevenue = curRevenue
             maxRevenueGenratedClass = class_
-    return "maximum revenue generated crop is"+maxRevenueGenratedClass if maxRevenueGenratedClass!="" else "Sorry We Cant Find Maximum revenue generated crop"
+    chart_data = pd.DataFrame(revenues,considered_classes)
+
+    return "maximum revenue generated crop is "+maxRevenueGenratedClass if maxRevenueGenratedClass!="" else "Sorry We Cant Find Maximum revenue generated crop",chart_data
 
 def main():
     st.title("Crop Predition")
@@ -40,15 +51,14 @@ def main():
     state=st.text_input("State","type here")
     district=st.text_input("district","type here")
     market=st.text_input("market","type here")
-    commodity=st.text_input("commodity","type here")
-    variety=st.text_input("variety","type here")
     temperature=st.text_input("temperature","type here")
     humidity=st.text_input("humidity","type here")
     ph=st.text_input("ph","type here")
     rainfall=st.text_input("rainfall","type here")
     if st.button('predict'):
-        crop = predict_crop(state,district,market,commodity,variety,temperature,humidity,ph,rainfall)
+        crop,chart_data = predict_crop(state,district,market,temperature,humidity,ph,rainfall)
         st.success(crop)
+        st.bar_chart(chart_data)
 
 if __name__=='__main__':
     main()
